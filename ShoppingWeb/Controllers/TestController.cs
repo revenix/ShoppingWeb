ac4cdd4e0754c96c716d050ac51d05ec663a3543
moises.sullca <moises.sullca@GYSCPU049>
2017-11-07 16:53:27 -0500

    Agregar archivos de proyecto.

---
+++ ShoppingWeb/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return PartialView("~/Views/Test/account.cshtml");
+        }
+    }
+}
